4429ab61331db506986585ded2fd565da7e9a445
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-09 15:44:53 -0500

    code cleanup

--- app.js
+++ app.js
@@ -23,21 +23,7 @@ const player = {
 
 //generates card number for image
 function randomCardNumber() {
-  const numbers = [
-    'A',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    'J',
-    'Q',
-    'K',
-  ];
+  const numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   const ranNum = Math.floor(Math.random() * numbers.length);
   let cardNum = numbers[ranNum];
   return cardNum;
@@ -56,35 +42,19 @@ function dealCards() {
   player.total = 0;
   dealer.total = 0;
 
-  let dealer1stCardNum = randomCardNumber();
-  let dealer1stCardSuit = randomCardSuit();
-  let dealer2ndCardNum = randomCardNumber();
-  let dealer2ndCardSuit = randomCardSuit();
-  let player1stCardNum = randomCardNumber();
-  let player1stCardSuit = randomCardSuit();
-  let player2ndCardNum = randomCardNumber();
-  let player2ndCardSuit = randomCardSuit();
+  let dealer1stCardNum = randomCardNumber(); let dealer1stCardSuit = randomCardSuit();
+  let dealer2ndCardNum = randomCardNumber(); let dealer2ndCardSuit = randomCardSuit();
+  let player1stCardNum = randomCardNumber(); let player1stCardSuit = randomCardSuit();
+  let player2ndCardNum = randomCardNumber(); let player2ndCardSuit = randomCardSuit();
 
   //resets total color
   $('#player-total').css('color', 'black');
   //changes img based on ranNum and ranChar
-  $('#dealer-card-1').attr(
-    'src',
-    'cards/' + dealer1stCardNum + dealer1stCardSuit + '.jpg'
-  );
-  $('#dealer-card-2').attr(
-    'src',
-    'cards/' + dealer2ndCardNum + dealer2ndCardSuit + '.jpg'
-  );
+  $('#dealer-card-1').attr('src','cards/' + dealer1stCardNum + dealer1stCardSuit + '.jpg');
+  $('#dealer-card-2').attr('src','cards/' + dealer2ndCardNum + dealer2ndCardSuit + '.jpg');
 
-  $('#player-card-1').attr(
-    'src',
-    'cards/' + player1stCardNum + player1stCardSuit + '.jpg'
-  );
-  $('#player-card-2').attr(
-    'src',
-    'cards/' + player2ndCardNum + player2ndCardSuit + '.jpg'
-  );
+  $('#player-card-1').attr('src','cards/' + player1stCardNum + player1stCardSuit + '.jpg');
+  $('#player-card-2').attr('src','cards/' + player2ndCardNum + player2ndCardSuit + '.jpg');
 
   //dealer total calc
   switch (dealer1stCardNum) {
@@ -179,10 +149,7 @@ function dealersTurn() {
 function hitMeDealer() {
   let dealerHitCardNum = randomCardNumber();
   let dealerHitCardSuit = randomCardSuit();
-  $('#player-card-1').attr(
-    'src',
-    'cards/' + dealerHitCardNum + dealerHitCardSuit + '.jpg'
-  );
+  $('#player-card-1').attr('src','cards/' + dealerHitCardNum + dealerHitCardSuit + '.jpg');
   switch (dealerHitCardNum) {
     case 'K':
       dealerHitCardNum = 10;
@@ -198,19 +165,14 @@ function hitMeDealer() {
       break;
   }
   // player.total += dealerHitCardNum
-  $('#dealer-total').text(
-    `Total: ${(dealer.total += parseInt(dealerHitCardNum))}`
-  );
+  $('#dealer-total').text(`Total: ${(dealer.total += parseInt(dealerHitCardNum))}`);
 }
 
 //function to draw another card
 function hitMe() {
   let playerHitCardNum = randomCardNumber();
   let playerHitCardSuit = randomCardSuit();
-  $('#player-card-1').attr(
-    'src',
-    'cards/' + playerHitCardNum + playerHitCardSuit + '.jpg'
-  );
+  $('#player-card-1').attr('src','cards/' + playerHitCardNum + playerHitCardSuit + '.jpg');
   switch (playerHitCardNum) {
     case 'K':
       playerHitCardNum = 10;
@@ -226,9 +188,7 @@ function hitMe() {
       break;
   }
   // player.total += playerHitCardNum
-  $('#player-total').text(
-    `Total: ${(player.total += parseInt(playerHitCardNum))}`
-  );
+  $('#player-total').text(`Total: ${(player.total += parseInt(playerHitCardNum))}`);
 }
 
 //decides who winner is
