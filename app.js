bb30daa2cdf67077e7d05dd30a1e1019aceebcd7
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-08 19:21:31 -0500

    game flow added

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+game flow:
+1. player is greeted with black jack rules, then closes the modal
+2. player presses start button
+3. cards are drawn
+4. player can either hit or stand
+5. once 21 or stand, dealer draws if needed
+6. game results displayed
+7. user can press start and we go back to 1.
+
+ */
