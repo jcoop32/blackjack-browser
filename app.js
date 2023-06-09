09239f32a90fe37062597b5247e35e90bc35a6a7
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-09 11:22:40 -0500

    game logic added

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ let exitGame = false;
 
 //generates random card
 function ranCardNum() {
+  $('#player-total').css('color', 'black');
   const numbersD = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   const ranNumD = Math.floor(Math.random() * numbersD.length);
   const charactersD = ['C', 'D', 'H', 'S'];
@@ -61,8 +62,27 @@ function ranCardNum() {
     'src',
     'cards/' + numbers2[ranNum2] + characters2[ranChar2] + '.jpg'
   );
-    
+
+  //dealer total calc
+  let dealerCard1 = numbersD[ranNumD]
+  switch (dealerCard1) {
+    case 'K':
+      dealerCard1 = 10;
+      break;
+    case 'J':
+      dealerCard1 = 10;
+      break;
+    case 'Q':
+      dealerCard1 = 10;
+      break;
+    case 'A':
+      dealerCard1 = 11;
+      break;
+  }
+  dealer.total += parseInt(dealerCard1);
+  $('#dealer-total').text(`Total: ${dealer.total}`)
   
+
   //player total calc
   let playerCard1 = numbers[ranNum];
   let playerCard2 = numbers2[ranNum2];
@@ -98,15 +118,86 @@ function ranCardNum() {
       break;
   }
   //adding number together and parsing strings that arent face cards
-  let playerTotal = parseInt(playerCard1) + parseInt(playerCard2);
+  player.total = parseInt(playerCard1) + parseInt(playerCard2);
   console.log('player card 1 after: ' + playerCard1);
   console.log('player card 2 after: ' + playerCard2);
   //changing total text
-  $('#player-total').text(`Total: ${playerTotal}`);
+  $('#player-total').text(`Total: ${player.total}`);
   // $('#play-btn').hide();
+}
+
+function hitMe(){
+  const numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  const ranNum = Math.floor(Math.random() * numbers.length);
+  const characters = ['C', 'D', 'H', 'S'];
+  const ranChar = Math.floor(Math.random() * characters.length);
+  $('#player-card-1').attr(
+    'src',
+    'cards/' + numbers[ranNum] + characters[ranChar] + '.jpg'
+  );
+  let playerCard = numbers[ranNum]
+  switch (playerCard) {
+    case 'K':
+      playerCard = 10;
+      break;
+    case 'Q':
+      playerCard = 10;
+      break;
+    case 'J':
+      playerCard = 10;
+      break;
+    case 'A':
+      playerCard = 11;
+      break;
+  }
+  // player.total += playerCard
+  $('#player-total').text(`Total: ${player.total += parseInt(playerCard)}`);
+}
+
+
+function gameLogic(){
+  if (player.total < 21 && dealer.total < 21 && player.total > dealer.total) {
+    $('#player-total').css('color', 'green');
+    $('#winner').text('Player wins');
+  }
+  if (player.total === 21) {
+    $('#player-total').css('color', 'green');
+    $('#winner').text('Player Wins with 21!');
+  }
+  if (dealer.total > 21) {
+    $('#player-total').css('color', 'green');
+    $('#winner').text('Player Wins, dealer busted');
+  }
+  if (dealer.total === 21) {
+    $('#winner').text(`Dealer has 21:(`);
+  }
+  if (player.total > 21) {
+    $('#winner').text(`Dealer Wins, player busted`);
+  }
+  if (dealer.total < 21 && player.total < 21 && dealer.total > player.total) {
+    $('#winner').text('Dealer Wins');
+  }
+  if (player.total === dealer.total) {
+    $('#winner').text('Its a Draw!');
+  }
+}
+
+
+function stand(){
+  gameLogic();
 }
 
 $(() => {
   //play-btn onClick call
   $('#play-btn').on('click', ranCardNum);
+  $('#hit').on('click', function (){
+    if (player.total < 21){
+      hitMe()
+    } 
+    if (player.total > 21){
+      $('#player-total').css('color', 'red');
+      stand();
+    }
+  })
+  $('#stand').on('click', stand)
 });
